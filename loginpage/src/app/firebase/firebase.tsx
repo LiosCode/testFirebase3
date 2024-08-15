3bd51fe034297b35e1f00281d1d5f50b9686a935
LiosCode <126961749+LiosCode@users.noreply.github.com>
2024-08-15 18:36:49 +0200

    set up database

--- loginpage/src/app/firebase/firebase.tsx
+++ loginpage/src/app/firebase/firebase.tsx
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-0x-1-2-3",
-  authDomain: "loginpage-b2b6c.firebaseapp.com",
-  projectId: "loginpage-b2b6c",
-  storageBucket: "loginpage-b2b6c.appspot.com",
-  messagingSenderId: "101010",
-  appId: "1:101010:web:101010",
-  measurementId: "G-101010",
-};
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const auth = getAuth(app);
