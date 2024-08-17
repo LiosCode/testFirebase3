0cb7e493ae4de7e2b3d4a1137b808ff0e3c2edde
LiosCode <126961749+LiosCode@users.noreply.github.com>
2024-08-17 19:52:24 +0200

    set up login page

--- loginpage/src/app/regrister/page.tsx
+++ loginpage/src/app/regrister/page.tsx
@@ -5,7 +5,7 @@ import {
     createUserWithEmailAndPassword,
     sendEmailVerification,
 } from "firebase/auth";
-import { auth } from "../../firebase";
+import { auth } from "./app/firebase/firebase";
 
 const Registerpage = () => {
     const [firstName, setFirstName] = useState("");
@@ -62,4 +62,69 @@ const Registerpage = () => {
 
     return (
         <div className="bg gardient-to-b from-gray-600 to-black justify-center items-center h-screen w-screen flex flex-col h-screen relative">
-           
+           <h2 className="text-2xl font-bold text-center mb-10">Register</h2>
+           <div className="p-5 border border-gray-300 rounded ">
+           <form onSubmit={handleRegister} className="space-y-6 px-6 pb-4">
+               <div className="flex space-x-4">
+                <div className="w-1/2">
+                <label htmlFor="firstName" className="text-sm font-medium block mb-2 text-gray-300">
+                    First Name</label>
+                <input type="text" 
+                id="firstName" 
+                placeholder="First Name" 
+                value={firstName} 
+                onChange={(e) => setFirstName(e.target.value)}
+                required className="border 2 outline-none sm:text-sm rounded-Lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"/>
+                </div>
+               </div>
+
+               <div className="flex space-x-4">
+                <div className="w-1/2">
+                <label htmlFor="lastName" className="text-sm font-medium block mb-2 text-gray-300">
+                    Last Name</label>
+                <input type="text" 
+                id="lastName" 
+                placeholder="Last Name" 
+                value={lastName} 
+                onChange={(e) => setLastName(e.target.value)}
+                required className="border 2 outline-none sm:text-sm rounded-Lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"/>
+                </div>
+               </div>
+
+
+               <div className="flex space-x-4">
+                <div className="w-1/2">
+                <label htmlFor="password" className="text-sm font-medium block mb-2 text-gray-300">
+                    password</label>
+                <input type="password" 
+                id="password" 
+                placeholder="password" 
+                value={password} 
+                onChange={(e) => setPassword(e.target.value)}
+                required className="border 2 outline-none sm:text-sm rounded-Lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"/>
+                </div>
+               </div>
+
+               <div className="flex space-x-4">
+                <div className="w-1/2">
+                <label htmlFor="confirmPassword" className="text-sm font-medium block mb-2 text-gray-300">
+                confirmPassword</label>
+                <input type="password" 
+                id="confirmPassword" 
+                placeholder="confirmPassword" 
+                value={confirmPassword} 
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                required className="border 2 outline-none sm:text-sm rounded-Lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 bg-gray-600 border-gray-500 placeholder-gray-400 text-white"/>
+                </div>
+               </div>
+               {error && <p className="text-red-500 text-sm font-medium">{error}</p>}
+               {message && <p className="text-green-500 text-sm font-medium">{message}</p>}
+               <button
+                type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                Sign up
+               </button>
+               </form>
+               </div>
+               </div>
+
+    )}
